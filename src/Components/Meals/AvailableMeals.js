f8d77eeab7cae2cf135a0331ab4de10d4266112b
ElderNobrega <eldernguerra.filho@hotmail.com>
2021-09-03 18:17:53 -0400

    created mealItem and Card components with styling

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+
 import classes from './AvailableMeals.module.css'
 
 const DUMMY_MEALS = [
   {
     id: 'm1',
-    name: 'Sushi',
+    name: 'Baked Sushi',
     description: 'Finest fish and veggies',
     price: 22.99,
   },
@@ -31,11 +34,13 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
 
-  const mealsList = DUMMY_MEALS.map( meal => <li>{meal.name}</li>)
+  const mealsList = DUMMY_MEALS.map( meal => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   )
 }
